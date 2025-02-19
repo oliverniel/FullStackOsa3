e771a3720c323840a7513414b91de72bb05e1904
Oliver Nielikäinen <olivernielikainen@gmail.com>
2025-02-19 10:36:05 +0200

    eka tuotantoversio

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length]- :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
